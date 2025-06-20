1e94f0310105d9f929f6a088e23e18270ea4c4e1
Doublado <nikolajjak@pm.me>
2025-06-20 14:01:41 +0200

    Enhance nuxt.config.ts by integrating TailwindCSS and PrimeVue modules, updating CSS imports, and configuring PrimeVue options for improved theming and functionality.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import Aura from '@primeuix/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils']
+  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@primevue/nuxt-module'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  primevue: {
+    usePrimeVue: true,
+    autoImport: true,
+    options: {
+      ripple: true,
+      inputStyle: 'filled',
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: 'system',
+          cssLayer: false,
+        }
+      }
+    },
+  },
 })
