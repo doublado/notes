0105a9ee8f9c58b209f193e0b9b57e7799a25f80
Doublado <nikolajjak@pm.me>
2025-06-20 14:01:31 +0200

    Add default layout and dashboard/index pages with basic structure

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "default",
+})
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-4xl font-bold">Hello World</h1>
+    <Button label="Verify" />
+  </div>
+</template>
