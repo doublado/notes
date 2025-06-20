c814b407126ad7a682ab8cfdf3a89d3258356f82
Doublado <nikolajjak@pm.me>
2025-06-21 00:27:12 +0200

    Update authentication middleware to redirect to the login page instead of the dashboard when no session is found, improving user flow for unauthenticated access.

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -4,7 +4,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
 	const { data: session } = await authClient.useSession(useFetch);
 	if (!session.value) {
 		if (to.path === "/dashboard") {
-			return navigateTo("/");
+			return navigateTo("/auth");
 		}
 	}
 });
